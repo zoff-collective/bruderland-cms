1ed50d07d5d0f3dbbd6436319ab88a3ddd178fde
Gustav Pursche <pursche@posteo.de>
2018-10-22 17:54:08 +0200

    Add first export

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bruderland-cms
+
+The backend interface for bruderland.netlify.com. It's based on wordpress and
+provides the content through the rest api.
---
+++ theme/functions.php
@@ -0,0 +1,27 @@
+<?php
+
+function bruderland_register_post_types() {
+  register_post_type('episodes',
+    array(
+      'labels' => array(
+        'name' => 'Episodes',
+        'singular_name' => 'Episode'
+        ),
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => array(
+          'slug' => 'episodes'
+        ),
+        'show_in_rest' => true,
+        'supports' => array(
+          'title',
+          'thumbnail',
+          'revisions',
+        )
+    )
+  );
+}
+
+add_action('init', 'bruderland_register_post_types');
+
+?>

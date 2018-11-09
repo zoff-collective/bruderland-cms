d17f09019e4c152bfccd51c9a7e1f2636cf73942
Gustav Pursche <pursche@posteo.de>
2018-11-09 14:57:07 +0100

    Trigger netlify builds on save_post

--- theme/functions.php
+++ theme/functions.php
@@ -22,6 +22,12 @@ function bruderland_register_post_types() {
   );
 }
 
+// see https://plugins.trac.wordpress.org/browser/wp-gatsby/trunk/class-wp-gatsby.php
+function trigger_netlify_deploy() {
+  wp_remote_post('https://api.netlify.com/build_hooks/5be58c5573f2cf648d1dacd5');
+}
+
 add_action('init', 'bruderland_register_post_types');
+add_action('save_post', 'trigger_netlify_deploy');
 
 ?>

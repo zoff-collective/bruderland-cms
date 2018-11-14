24ca3eab5e09d955eb515a2089c08b3321ba08e6
Gustav Pursche <pursche@posteo.de>
2018-11-14 10:02:26 +0100

    Cleanup admin interface

--- theme/functions.php
+++ theme/functions.php
@@ -27,9 +27,17 @@ function trigger_netlify_deploy() {
   wp_remote_post('https://api.netlify.com/build_hooks/5be58c5573f2cf648d1dacd5');
 }
 
+function cleanup_admin() {
+  remove_menu_page('edit.php');
+  remove_menu_page('edit.php?post_type=page');
+  remove_menu_page('edit-comments.php');
+  remove_menu_page('upload.php');
+}
+
 add_theme_support('post-thumbnails');
 
 add_action('init', 'bruderland_register_post_types');
 add_action('save_post', 'trigger_netlify_deploy');
+add_action('admin_menu','cleanup_admin');
 
 ?>

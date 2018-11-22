b6b1fc50bcee8ee93543d8507c4f1338824ecad9
Gustav Pursche <pursche@posteo.de>
2018-11-22 16:37:56 +0100

    Show media library in sidebar

--- theme/functions.php
+++ theme/functions.php
@@ -69,7 +69,6 @@ function cleanup_admin() {
   remove_menu_page('edit.php');
   remove_menu_page('edit.php?post_type=page');
   remove_menu_page('edit-comments.php');
-  remove_menu_page('upload.php');
 }
 
 // Register custom toolbars
